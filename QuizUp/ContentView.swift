34e20bcb5ecebbd6817f3e96032a9950f220fa16
ChayanitL <chayanit9336@aten.co.th>
2024-01-05 18:17:03 +0700

    Initial Commit

---
+++ QuizUp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  QuizUp
+//
+//  Created by ชญานิษฐ์ โลหะกิจจา on 5/1/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuizUp/QuizUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizUpApp.swift
+//  QuizUp
+//
+//  Created by ชญานิษฐ์ โลหะกิจจา on 5/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct QuizUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
